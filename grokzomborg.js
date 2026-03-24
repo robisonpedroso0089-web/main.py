fde51a3a7e847ea094253b72fbe2eb2db6e907fc
robisonpedroso0089-web <robisonpedroso0089@gmail.com>
2026-03-24 04:06:54 -0300

    Create grokzomborg.js

---
+++ grokzomborg.js
@@ -0,0 +1,16 @@
+# 🦙 Ollama JavaScript Library
+
+[![npm version](https://img.shields.io/npm/v/ollama.svg)](https://www.npmjs.com/package/ollama)
+[![Stars](https://img.shields.io/github/stars/robisonpedroso0089-web/ollama-js)](https://github.com/robisonpedroso0089-web/ollama-js/stargazers)
+[![License](https://img.shields.io/github/license/robisonpedroso0089-web/ollama-js)](LICENSE)
+
+A biblioteca JavaScript mais fácil e completa para integrar seus projetos com **Ollama** (LLMs locais e na nuvem).
+
+Funciona perfeitamente no **Node.js** e no **Navegador**.
+
+---
+
+## 🚀 Instalação
+
+```bash
+npm install ollama
